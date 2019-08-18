23673c66610b057127524c3aef7dff185efe4000
gordonpn <gordon.pn6@gmail.com>
2019-08-18 11:24:50 -0400

    clean up

--- src/main/java/com/rfdhd/scraper/App.java
+++ src/main/java/com/rfdhd/scraper/App.java
@@ -3,7 +3,6 @@ package com.rfdhd.scraper;
 import com.rfdhd.scraper.model.ThreadInfo;
 import com.rfdhd.scraper.services.Scraper;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class App {
@@ -11,8 +10,10 @@ public class App {
     public static void main(String[] args) {
         // todo move this hardcoded parameter into a config file to read from
         Scraper scraper = new Scraper(10);
-        Map<String, ThreadInfo> rawThreadsMap = new HashMap<>();
+        Map<String, ThreadInfo> rawThreadsMap;
+        Map<String, ThreadInfo> filteredThreads;
 
         rawThreadsMap = scraper.getThreadsMap();
+        filteredThreads = scraper.filter(rawThreadsMap);
     }
 }
